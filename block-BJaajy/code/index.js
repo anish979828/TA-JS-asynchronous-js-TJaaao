9da107b9f7275c94d3765db1cabb02171ccc8603
ANISH KUMAR GIRI <girianish2004@gmail.com>
2023-02-21 00:33:23 +0530

    More about promisses

--- block-BJaajy/code/index.js
+++ block-BJaajy/code/index.js
@@ -1,17 +1,29 @@
-// - Create four promises that resolve after 1, 2, 3 and 4 seconds with a random value. Using `Promise.all` log the value of each promise that it resolved with.tt
+// - Create four promises that resolve after 1, 2, 3 and 4 seconds with a random value. Using `Promise.all` log the value of each promise that it resolved with it.
 
 let first = Promise.resolve("Hey");
-let second = Promise.resolve("Anish!")
-let third = Promise.resolve("How are u?")
-let fourth = Promise.resolve("All good!")
+let second = Promise.resolve("Anish!");
+let third = Promise.resolve("How are u?");
+let fourth = Promise.resolve("All good!");
 
-Promise.all([first,second,third,fourth]).then(console.log);
+Promise.all([first,second,third,fourth]).then(val => val.forEach(val => console.log(val)));
+
 // - Create a list of 5 Github usernames in an array and using `Promise.all` get access to the data of each user from GitHub API. Log the number of followers of each user.
+let username = ["anish979828","aditya2z","prank7","ritesh22201"];
+
+Promise.all(username).then(val => {
+    val.forEach(user => {
+        fetch(`https://api.github.com/users/${user}`).then(val => val.json()).then(val => console.log(val.followers))
+    })
+});
 
 // - Use `Promise.race` to see which API resolves faster from the given list of URLs. Log the object you get from the promise that is resolved faster.
 
 //   - https://random.dog/woof.json
 //   - https://aws.random.cat/meow
+
+let promise1 = Promise.resolve("")
+
+Promise.race([])
 
 // - Use `Promise.allSettled` to log the value of each promise from the given list of promises. And also check if `Promise.all` works with `one`, `two` and `three` or not
 
